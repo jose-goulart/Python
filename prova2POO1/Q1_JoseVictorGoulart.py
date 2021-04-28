23b13eccfc84ab12593c3307368d45f61c2c97e4
VictorGs2 <xxvictogoulart2000@gmail.com>
2021-04-28 00:15:58 -0300

    finalizada Q1 e Q2

--- prova2POO1/Q1_JoseVictorGoulart.py
+++ prova2POO1/Q1_JoseVictorGoulart.py
@@ -1,17 +1,7 @@
 # Nome: José Victor Goulart dos Santos
 # Matrícula: 20200412
-matriz = []
-diagonalP1 = 0
-diagonalP2 = 0
-diagonalInvP1 = 0
-diagonalInvP2 = 0
-horizontalP1 = 0
-horizontalP2 = 0
-verticalP1 = 0
-verticalP2 = 0
-highestP1 = 0
-highestP2 = 0
-firstPos = 0
+from random import randint
+
 while True:
     print("1 - Preencher o tabuleiro lendo valores do teclado")
     print("2 - Preencher o tabuleiro randomicamente")
@@ -19,31 +9,34 @@ while True:
     option = int(input("Digite o número para uma das opções acima: "))
 
     if(option == 1):
-        
-        for i in range(4):
+        matriz = []
+        winP1 = 0
+        winP2 = 0
+
+        for i in range(10):
             matriz.append([int(x) for x in input().split()])
         # Verifica a repetição de elemento na matriz
-        for j in range(4):
-            for k in range(4):
+        for j in range(10):
+            for k in range(10):
                 if(matriz[j][k] == 1):
                     count = 0
                     # Verifica se tem cinco elementos 1 repetidos na horizontal
-                    if(4 - k >= 3):
-                        for l in range(4 - k):
+                    if(10 - k >= 5):
+                        for l in range(10 - k):
                             if(matriz[j][k + l] == 1):
                                 count += 1
-                                if(count >= horizontalP1):
-                                    horizontalP1 = count
+                                if(count >= 5):
+                                    winP1 = 1
                             else:
                                 count = 0
                     # Verifica se tem cinco elementos 1 repetidos na vertical
                     count = 0
-                    if(4 - j >= 3):
-                        for c in range(4 - j):
+                    if(10 - j >= 5):
+                        for c in range(10 - j):
                             if(matriz[j + c][k] == 1):
                                 count += 1
-                                if(count >= verticalP1):
-                                    verticalP1 = count
+                                if(count >= 5):
+                                    winP1 = 1
                             else:
                                 count = 0
 
@@ -51,55 +44,192 @@ while True:
                     count = 0
                     # print(k)
                     # print(j)
-                    for dp in range(4 - k):
-                        if(j + dp < 4):
-                            if(matriz[j + dp][k + dp] == 1):
+                    if(10 - k >= 5):
+                        for dp in range(10 - k):
+                            if(j + dp < 10):
+                                if(matriz[j + dp][k + dp] == 1):
+                                    count += 1
+                                    if(count >= 5):
+                                        winP1 = 1
+                                else:
+                                    count = 0
+
+                    # Verifica se tem cinco elementos 1 repetidos na diagonal inversa
+                    count = 0
+                    # if(k + 1 >= 5):
+                    for di in range(k + 1):
+                        if(j + di < 10):
+                            if(matriz[j + di][k - di] == 1):
                                 count += 1
-                                if(count >= diagonalP1):
-                                    diagonalP1 = count
+                                if(count >= 5):
+                                    winP1 = 1
                             else:
                                 count = 0
-
-                    # Verifica se tem cinco elementos 1 repetidos na diagonal inversa        
-                    for di in range(4 - k):
-                        if(matriz[j - di][k - di] == 1):
-                            count += 1
-                            if(count >= diagonalInvP1):
-                                diagonalInvP1 = count
-                        else:
-                            count = 0
 
                 if(matriz[j][k] == 2):
                     count = 0
-                    # Verifica se tem cinco elementos 2 repetidos na horizontal
-                    if(4 - k >= 3):
-                        for l in range(4 - k):
+                    # Verifica se tem cinco elementos 1 repetidos na horizontal
+                    if(10 - k >= 5):
+                        for l in range(10 - k):
                             if(matriz[j][k + l] == 2):
                                 count += 1
-                                if(count >= horizontalP2):
-                                    horizontalP2 = count
+                                if(count >= 5):
+                                    winP2 = 1
                             else:
                                 count = 0
-                    # Verifica se tem cinco elementos 2 repetidos na vertical
-                    if(4 - k >= 3):
-                        count = 0
-                        for c in range(4 - j):
+                    # Verifica se tem cinco elementos 1 repetidos na vertical
+                    count = 0
+                    if(10 - j >= 5):
+                        for c in range(10 - j):
                             if(matriz[j + c][k] == 2):
                                 count += 1
-                                if(count >= verticalP2):
-                                    verticalP2 = count
+                                if(count >= 5):
+                                    winP2 = 1
                             else:
                                 count = 0
 
-    # elif(option == 2):
+                    # Verifica se tem cinco elementos 1 repetidos na diagonal principal
+                    count = 0
+                    # print(k)
+                    # print(j)
+                    if(10 - k >= 5):
+                        for dp in range(10 - k):
+                            if(j + dp < 10):
+                                if(matriz[j + dp][k + dp] == 2):
+                                    count += 1
+                                    if(count >= 5):
+                                        winP2 = 1
+                                else:
+                                    count = 0
+
+                    # Verifica se tem cinco elementos 1 repetidos na diagonal inversa
+                    count = 0
+                    # if(k + 1 >= 5):
+                    for di in range(k + 1):
+                        if(j + di < 10):
+                            if(matriz[j + di][k - di] == 2):
+                                count += 1
+                                if(count >= 5):
+                                    winP2 = 1
+                            else:
+                                count = 0
+        if(winP1 == 1 and winP2 == 0):
+            print("1")
+        elif(winP2 == 1 and winP1 == 0):
+            print("2")
+        else:
+            print("Não há um vencedor")
+    elif(option == 2):
+        matriz = []
+        winP1 = 0
+        winP2 = 0
+        for i in range(10):
+            matriz.append([])
+            for j in range(10):
+                matriz[i].append(randint(0, 2))
+        for j in range(10):
+            for k in range(10):
+                if(matriz[j][k] == 1):
+                    count = 0
+                    # Verifica se tem cinco elementos 1 repetidos na horizontal
+                    if(10 - k >= 5):
+                        for l in range(10 - k):
+                            if(matriz[j][k + l] == 1):
+                                count += 1
+                                if(count >= 5):
+                                    winP1 = 1
+                            else:
+                                count = 0
+                    # Verifica se tem cinco elementos 1 repetidos na vertical
+                    count = 0
+                    if(10 - j >= 5):
+                        for c in range(10 - j):
+                            if(matriz[j + c][k] == 1):
+                                count += 1
+                                if(count >= 5):
+                                    winP1 = 1
+                            else:
+                                count = 0
+
+                    # Verifica se tem cinco elementos 1 repetidos na diagonal principal
+                    count = 0
+                    # print(k)
+                    # print(j)
+                    if(10 - k >= 5):
+                        for dp in range(10 - k):
+                            if(j + dp < 10):
+                                if(matriz[j + dp][k + dp] == 1):
+                                    count += 1
+                                    if(count >= 5):
+                                        winP1 = 1
+                                else:
+                                    count = 0
+
+                    # Verifica se tem cinco elementos 1 repetidos na diagonal inversa
+                    count = 0
+                    # if(k + 1 >= 5):
+                    for di in range(k + 1):
+                        if(j + di < 10):
+                            if(matriz[j + di][k - di] == 1):
+                                count += 1
+                                if(count >= 5):
+                                    winP1 = 1
+                            else:
+                                count = 0
+
+                if(matriz[j][k] == 2):
+                    count = 0
+                    # Verifica se tem cinco elementos 1 repetidos na horizontal
+                    if(10 - k >= 5):
+                        for l in range(10 - k):
+                            if(matriz[j][k + l] == 2):
+                                count += 1
+                                if(count >= 5):
+                                    winP2 = 1
+                            else:
+                                count = 0
+                    # Verifica se tem cinco elementos 1 repetidos na vertical
+                    count = 0
+                    if(10 - j >= 5):
+                        for c in range(10 - j):
+                            if(matriz[j + c][k] == 2):
+                                count += 1
+                                if(count >= 5):
+                                    winP2 = 1
+                            else:
+                                count = 0
+
+                    # Verifica se tem cinco elementos 1 repetidos na diagonal principal
+                    count = 0
+                    # print(k)
+                    # print(j)
+                    if(10 - k >= 5):
+                        for dp in range(10 - k):
+                            if(j + dp < 10):
+                                if(matriz[j + dp][k + dp] == 2):
+                                    count += 1
+                                    if(count >= 5):
+                                        winP2 = 1
+                                else:
+                                    count = 0
+
+                    # Verifica se tem cinco elementos 1 repetidos na diagonal inversa
+                    count = 0
+                    # if(k + 1 >= 5):
+                    for di in range(k + 1):
+                        if(j + di < 10):
+                            if(matriz[j + di][k - di] == 2):
+                                count += 1
+                                if(count >= 5):
+                                    winP2 = 1
+                            else:
+                                count = 0
+        if(winP1 == 1 and winP2 == 0):
+            print("1")
+        elif(winP2 == 1 and winP1 == 0):
+            print("2")
+        else:
+            print("Não há um vencedor")
+
     elif(option == 3):
         break
-    print(horizontalP1)
-    print(verticalP1)
-    # print(diagonalP1)
-    # print(diagonalInvP1)
-    print(horizontalP2)
-    print(verticalP2)
-    # print(diagonalP2)
-    # print(diagonalInvP2)
-    print(matriz)
--- prova2POO1/Q2_JoseVictorGoulart.py
+++ prova2POO1/Q2_JoseVictorGoulart.py
@@ -1,2 +1,27 @@
 # Nome: José Victor Goulart dos Santos
 # Matrícula: 20200412
+def dist(x, y):
+    avg = (abs(x) + abs(y)) / 2
+    return avg
+i = 0
+dict = {}
+arrayDict = []
+n = int(input("Digite a quantidade de flechas lançadas: "))
+while(i < n):
+    x, y = [int(x) for x in input().split()]
+    dict['x'] = x
+    dict['y'] = y
+    arrayDict.append(dict.copy())
+    i += 1
+i = 0
+sum = 0
+temp = 0
+while(i < len(arrayDict)):
+    previous = dist(arrayDict[i]["x"], arrayDict[i]["y"])
+    if(i < len(arrayDict) - 1):
+        next = dist(arrayDict[i + 1]["x"], arrayDict[i + 1]["y"])
+    if(previous <= next):
+        sum = sum + temp
+        temp += 1
+    i += 1
+print(sum)
